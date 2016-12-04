8c3467d47bbfa1d6ec98a400ae584a934134eb5e
Matt Glover <matthewglover@me.com>
2016-12-04 15:27:55 +0000

    feat(user-details): add github api call using access_token from session variable

--- routes/user_details.js
+++ routes/user_details.js
@@ -1,4 +1,14 @@
+const request = require('request');
 
+const getUserDetails = (accessToken, cb) => {
+  request.get({
+    url: 'https://api.github.com/user',
+    headers: {
+      'User-Agent': 'oauth-workshop',
+      Authorization: `token ${accessToken}`,
+    },
+  }, cb);
+};
 
 module.exports = {
   method: 'GET',
@@ -6,7 +16,10 @@ module.exports = {
   config: {
     auth: 'session',
     handler(req, reply) {
-      reply(req.auth.credentials);
+      getUserDetails(req.auth.credentials.access_token, (err, response, body) => {
+        if (err) throw err;
+        reply(JSON.parse(body));
+      });
     },
   },
 };
